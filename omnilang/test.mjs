e90a18468df72d5dab2418cfbe7be287435b2310
stephen h <136494426+100percentexistant@users.noreply.github.com>
2023-08-24 18:29:02 -0400

    OmniLang - Test

---
+++ omnilang/test.mjs
@@ -0,0 +1,13 @@
+import { default as ol } from "./omnilang.mjs";
+
+/*
+WHAT IT DOES:
+Loads the parent folder's main database (db.sqlite3)
+Assigns to "b" the result of all users whose type is "2".
+*/
+var cmd = await ol(`
+@~~join(., ../db.sqlite3)
+b = ~(*)::users ? type == 2
+`);
+
+console.log(cmd.b);
